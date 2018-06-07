98bd62f1c0ac7bb2827f8558478468aa55b27bb4
Ivan Tsui <ixt3042@g.rit.edu>
2018-06-07 14:37:00 -0400

    Added config files for players and ranking

--- handball/settings.py
+++ handball/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'players.apps.PlayersConfig',
+    'ranking.apps.RankingConfig',
 ]
 
 MIDDLEWARE = [
